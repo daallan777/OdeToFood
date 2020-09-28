1c79240889b941ef0f9d40b63297b55ff1c1f4c0
Krishnan Narayana Swamy <krnaraya@redhat.com>
2020-09-28 13:51:33 +1000

    ud:wq!

--- update.ps1
+++ update.ps1
@@ -5,13 +5,9 @@ write-host "----------------------------`n"
 $p = Get-Process -Name "dotnet"
 
 if ($p) {
-  # try gracefully first
-  $p.CloseMainWindow()
-  # kill after five seconds
-  Sleep 5
-  if (!$p.HasExited) {
+
     $p | Stop-Process -Force
-  }
+
 }
 
 write-host "`n----------------------------"
